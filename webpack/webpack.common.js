10c3cce01c6bc63a152ce9ce386df003ff62a2ad
laptop <the.red.lemon@yandex.ru>
2024-03-17 12:37:25 +0300

    refactor init files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,18 +2,16 @@ const HTMLWebpackPlugins = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
-const webpack = require('webpack'); //подключаем webpack для использования встроенного плагина EnvironmentPlugin
+const webpack = require('webpack');
 
-//в зависимости от того, какой скрипт мы запустили
-// переменная production получит либо false, либо true
 const production = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),//путь до папки src изменился
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
   output: {
-    path: path.resolve(__dirname, '..', './dist'), //путь до папки dist изменился
+    path: path.resolve(__dirname, '..', './dist'),
     filename: production
-      ? 'static/scripts/[name].[contenthash].js'// добавляем хеш к имени файла, если запускаем в режиме production
+      ? 'static/scripts/[name].[contenthash].js'
       : 'static/scripts/[name].js',
     publicPath: '/',
   },
@@ -36,13 +34,6 @@ module.exports = {
         },
       },
       {
-        test: /\.(woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/fonts/[hash][ext][query]',
-        },
-      },
-      {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
         use: ['@svgr/webpack', 'url-loader'],
@@ -50,7 +41,6 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          //в режиме production создаём физический файл в папке dist, в dev режиме добавляем стили в тег style в html-файле
           production ? MiniCssExtractPlugin.loader : 'style-loader',
           {
             loader: 'css-loader',
@@ -69,33 +59,21 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [
-      '.js',
-      '.jsx',
-      '.ts',
-      '.tsx',
-      '.json',
-      '.css',
-      '.scss',
-      '.png',
-      '.svg',
-      '.jpg'
-    ],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css', '.png', '.svg', '.jpg'],
     alias: {
       '@components': path.resolve(__dirname, '..', './src/components'),
     }
   },
   plugins: [
     new HTMLWebpackPlugins({
-      template: path.resolve(__dirname, '..', './public/index.html'), //путь до папки public изменился
+      template: path.resolve(__dirname, '..', './public/index.html'),
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'static/styles/[name].[contenthash].css'
     }),
-    //Плагин позволяет установить переменные окружения, можно переопределить переменную из блока script файла package.json
     new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development', // значение по умолчанию 'development', если переменная process.env.NODE_ENV не передана при вызове сборки
+      NODE_ENV: 'development',
     }),
   ],
 };
